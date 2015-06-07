882668945698c071edb9ac0a260bff68b1efef7c
Piotr Goryl <piotr.goryl@uj.edu.pl>
2015-06-07 17:07:31 +0200

    Initial examples project

---
+++ lecture-example/scripts/hello-world-function.js
@@ -0,0 +1,18 @@
+﻿//Named function
+function hello_world(id) { // Scope definistions by curly brackets
+    if (!id) { 
+        alert('Hello world!');
+    } else {
+        document.getElementById(id).innerHTML = "Hello World";
+    }
+}
+
+/* Function as variable!
+*/
+var bye_world = function(id) {
+    if (!id) {
+        alert('Bye world!');
+    } else {
+        document.getElementById(id).innerHTML = "By World";
+    }
+}
