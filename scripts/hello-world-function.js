b69bdd581ce2a23e025658294e63ecb94e71458d
Piotr Goryl <piotr.goryl@uj.edu.pl>
2015-06-08 14:28:54 +0200

    importing examples for the first part of lecture

---
+++ scripts/hello-world-function.js
@@ -0,0 +1,22 @@
+﻿//Named function
+function hello_world(id) { // Scope definistions by curly brackets
+    if (!id) { //if statement similar to C/C++
+        alert('Hello world!');
+    } else {
+        document.getElementById(id).innerHTML = "Hello World";
+    }
+}
+
+/* anonymous function assigned 
+   to a variable
+*/
+var bye_world = function(id) {
+    if (!id) {
+        alert('Bye world!');
+    } else {
+        document.getElementById(id).innerHTML = "Bye World";
+    }
+}
+
+//yes, functions are objects
+var hw = hello_world //hw is  variable of type function which value is hellow world function object
---
+++ scripts/variables.js
@@ -0,0 +1,60 @@
+﻿var tmp = "some temporary object";
+
+function scope_test1(a) {
+    var tmp;
+    tmp = a;
+   
+}
+
+scope_test1("scope test")
+console.log(tmp);
+
+function scope_test2(a) {    
+    tmp = a;
+}
+
+scope_test2("scope test")
+console.log(tmp);
+
+function scope_test3(a) {
+    tmp = a;
+    var tmp;
+}
+
+scope_test3("scope test 3")
+console.log(tmp);
+
+var text = "Some primitive string";
+var pi = 3.14; //this is number
+
+var brush = { color: "green", pattern: "dots" };
+var shopping_list = ["apples", "milk", "matches"];
+
+var brush2 = brush;
+brush2.color = "red";
+
+console.log(brush.color);
+
+var text2 = text;
+text2 = "changed text";
+console.log(text);
+
+function this_is_variable_too() {
+    console.log("function is a variable");
+}
+
+this_is_variable_too();
+
+this_is_variable_too = function () {
+    console.log("one can change its value");
+};
+
+this_is_variable_too();
+
+(function () {
+    var text = "I will not overwrite global variable";
+    var pi = "Pi is firend of Sigma."
+    console.log(text);
+    console.log("Text from the outside scope:" + this.text);    
+})();
+
